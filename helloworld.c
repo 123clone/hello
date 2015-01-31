f7991f0fd1120d68741542cbb22ba638655580a0
Nick Clone <e175447@trbvm.com>
2015-01-31 10:19:17 +0700

    EDIT prototype of Sum2Num function

--- helloworld.c
+++ helloworld.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <math.h>
 
-function Diff2Num(int a, int b){
+function Diff2Num(int x, int y){
 	return(abs(a -b));
+}
+
+function Sum2Num(int, int);
+
+int main() {
+	printf("Hello world!!!");
 }
 
 function Sum2Num(int a, int b){
     return(a + b);
-}
-
-int main() {
-	printf("Hello world!");
 }
